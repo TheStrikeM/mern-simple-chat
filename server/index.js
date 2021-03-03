ca0f39c8f9b81741a9a390962a4022b9dcf6e2d6
Danil <dinkodanil@gmail.com>
2021-03-03 19:21:43 +0300

    Setting index.js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,29 @@
 import express from 'express'
 import server from 'http'
+import mongoose from 'mongoose'
+import config from 'config'
 import { Server } from 'socket.io'
 
 
 const app = express()
+const PORT = config.get("PORT") || 5000
 
 server.Server(app)
-const io = new Server(server)
+export const io = new Server(server)
 
 app.use(express.json())
 
-const start = () => {
 
+const start = async () => {
+    try {
+        await mongoose.connect(config.get("DB_CONNECT"))
+
+        app.listen(PORT, () => {
+            console.log(`Server started in PORT - ${PORT}`)
+        })
+    } catch (e) {
+        throw new Error("Error in index.js")
+    }
 }
+
+start()
